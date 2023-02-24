dca522fddffc4f03bb09c37877d84ad3ed0ff201
SebastianFia <fiasebastian.04@gmail.com>
2023-02-24 20:59:02 +0100

    implemented graceful shutdown for thread_pool

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -8,18 +8,23 @@ type SendableReceiver = Arc<Mutex<mpsc::Receiver<Job>>>;
 
 struct Worker {
     id: usize,
-    thread: JoinHandle<()>,
+    thread: Option<JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: SendableReceiver) -> Self {
-        let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+    fn new(id: usize, receiver: SendableReceiver) -> Self  {
+        let thread = Some(thread::spawn(move || loop {
+            let job = match receiver.lock().unwrap().recv() {
+                Err(_) => {
+                    break    
+                }
+                Ok(job) => job,
+            };
 
             println!("Worker {} got a job, start execution", id);
 
             job();
-        });
+        }));
 
         Worker{id, thread}
     }
@@ -27,7 +32,7 @@ impl Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
@@ -35,6 +40,7 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
+        let sender = Some(sender);
         let receiver: SendableReceiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
 
@@ -49,6 +55,18 @@ impl ThreadPool {
     where F: FnOnce() + Send + 'static 
     {
        let job: Job = Box::new(f);
-       self.sender.send(job).unwrap();
+       self.sender.as_mut().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in self.workers.iter_mut() {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        };
     }
 }
